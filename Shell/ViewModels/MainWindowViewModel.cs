5ac6921cea50e078db6b817f5041bb5d7c534317
GalaxyMiku <GalaxyMiku@GALAXY>
2023-09-08 10:57:16 +0800

    添加项目文件。

---
+++ Shell/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell.ViewModels
+{
+    internal class MainWindowViewModel:BindableBase
+    {
+        private string title="上位机开发";
+        public string Title
+        { 
+            get { return title; } 
+            set {  title = value;RaisePropertyChanged(); } 
+        
+        }
+    }
+}
